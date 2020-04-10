41764b2a941e4fc3739e5039f5acb2e056cb87bc
Helder77270 <34067975+Helder77270@users.noreply.github.com>
2020-04-10 10:20:23 +0100

    Exo 8

--- HelloWorld.py
+++ HelloWorld.py
@@ -43,7 +43,7 @@ def lexicographicOrder():
     if word1 == word2:
         print("Ce sont les même mots :) ")
         return 0
-    if word1 < word2:
+    elif word1 < word2:
         print("Le mot " + word1 + " est le plus petit dans l\'ordre lexicographique")
     else:
         print("Le mot " + word2 + " est le plus petit dans l\'ordre lexicographique")
@@ -56,19 +56,54 @@ def enceintePression(pSeuil, vSeuil):
     if pSeuil > 2.3 and vSeuil > 7.41:
         print("Arrêt immédiat")
         return 0
-    if pSeuil > 2.3 and vSeuil <= 7.41:
+    elif pSeuil > 2.3 and vSeuil <= 7.41:
         print("Augmentez le volume de l'enceinte")
         return 1
-    if pSeuil <= 2.3 and vSeuil > 7.41:
+    elif pSeuil <= 2.3 and vSeuil > 7.41:
         print("Diminuez le volume de l’enceinte")
         return 2
     print("Tout va bien")
 
-enceintePression(2.4 , 7.42)
-enceintePression(2.4 , 7.40)
-enceintePression(2.2 , 7.42)
-enceintePression(2.2 , 7.40)
+### enceintePression(2.4 , 7.42)
+### enceintePression(2.4 , 7.40)
+### enceintePression(2.2 , 7.42)
+### enceintePression(2.2 , 7.40)
 
+# PDF EXO 6
 
+def checkIfOnlyOneArobase(chaine):
+    cpt = 0
+    for i in range(len(chaine)):
+        if chaine[i] == '@':
+            cpt += 1
 
+    if cpt == 1:
+       # print("OK")
+        return True
+    else:
+       # print("NOT OK")
+        return False
 
+def checkIfMail(chaine):
+    if chaine.endswith('.com') and checkIfOnlyOneArobase(chaine):
+        print("E-mail valide !")
+    else:
+        print("E-mail invalide !")
+
+### checkIfMail("helder.salvador@yahoo.com")
+
+# PDF EXO 7
+
+def boucleX10():
+    for i in range(1,11):
+        print("Message " + str(i))
+
+### boucleX10()
+
+# PDF EXO 8
+
+def boucleLetter(word):
+    for i in range(len(word)):
+        print(word[i])
+
+### boucleLetter("Confinement")
